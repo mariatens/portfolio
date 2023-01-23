1723b1027bcdc055282b86d3b1723483295b0a27
mtens17 <mtens17@gmail.com>
2023-01-23 13:19:31 +0000

    Add new icons

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,13 +1,18 @@
 export const SocialData=[
     {
         platform:"Linkedin",
-        // icon: require("../../assets/icons/linkedin.png"),
+        icon: require("../../assets/icons/linkedin.png"),
         link:"https://www.linkedin.com/in/maria-ten/",
     },
     {
         platform:"Github",
         icon: require("../../assets/icons/github.png"),
         link:"https://github.com/mariatens",
+    },
+    {
+        platform:"Email",
+        icon: require("../../assets/icons/email.png"),
+        link:"mailto:mtens17@gmail.com",
     },
 
 ];
