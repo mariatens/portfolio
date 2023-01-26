4e6b71d5031a8cb2af60c717378697bda7528f80
mtens17 <mtens17@gmail.com>
2023-01-26 19:07:46 +0000

    Add project

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,11 +1,21 @@
 export const ProjectData = [
   {
+    id: 0,
+    title: 'Chronoline - In Progress', 
+    about: 'Chronoline is a game where the player needs to place a card in the correct chronoligical position of a timeline',
+    tags: ['React', 'TypeScript', 'Jest', 'HTML', 'CSS'],
+    demo: 'https://chronoline-game.netlify.app/',
+    github: 'https://github.com/lsimonar/chronoline',
+    image: require('../../assets/chronoline.png')
+
+  },
+  {
     id: 1,
     title: 'TV Shows Project',
     about:
       'The TV Shows webpage fetches data from the TV Maze API and shows it in a compelling way using React and Typescript. It allows the user to choose from different TV shows and filter for specific episodes',
-    tags: ['React', 'TypeScript', 'HTML', 'CSS'],
-    url: 'https://tv-shows-project-alli-maria.netlify.app/',
+    tags: ['React', 'TypeScript', 'Jest', 'HTML', 'CSS'],
+    demo: 'https://tv-shows-project-alli-maria.netlify.app/',
     github: 'https://github.com/mariatens/tv-shows-project',
     image: require('../../assets/tvshows.jpeg'),
   },
@@ -14,7 +24,7 @@ export const ProjectData = [
     title: 'Paste Bin',
     about:
       'Paste bin with functionality to comment on pastes and delete pastes/comments',
-    tags: ['React', 'Typescript', 'Node', 'Express', 'PostgreSQL'],
+    tags: ['React', 'Typescript', 'Jest', 'Node', 'Express', 'PostgreSQL'],
     demo: 'https://github.com/ThantZinPhyo-OP/Portfolio-Website',
     github: 'https://github.com/DidierOsindero/paste-bin-frontend',
     image: require('../../assets/pastebin.png'),
@@ -24,7 +34,7 @@ export const ProjectData = [
     title: 'To do app',
     about:
       'App with functionality to add, delete, edit and mark To Dos as complete',
-    tags: ['React', 'Typescript', 'Node', 'Express', 'PostgreSQL'],
+    tags: ['React', 'Typescript', 'Jest', 'Node', 'Express', 'PostgreSQL'],
     demo: 'https://mariatens-todo.netlify.app/',
     github: 'https://github.com/mariatens/todo-front-end',
     image: require('../../assets/todoapp.png'),
